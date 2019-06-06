225733822d7047c67328c45a6fcaca25ec84a511
mjachowdhury <study.computing2016@gmail.com>
2019-06-06 13:24:03 +0100

    adding 3rd year interactiveDataVisualization module

---
+++ InteractiveDataVisualization/assignment2/main.js
@@ -0,0 +1,16 @@
+var circle = function() {
+	var bodySelection = d3.select("body");
+
+var svgSelection = bodySelection.append("svg")
+	.attr("width",50)
+	.attr("height",50)
+
+var circleSelection = svgSelection.append("circle")	
+	.attr("cx", 25)
+	.attr("cy", 25)
+	.attr("r", 25)
+	.style("fill","purple");
+
+}
+
+circle();
